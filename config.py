3490ba986dea647ec74ea41d72e8969673a9ab0a
UltroidVenom <80195199+UltroidVenom@users.noreply.github.com>
2021-05-10 13:17:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ load_dotenv()
 que = {}
 SESSION_NAME = "AQBfEvGYgijv_72_HsGzMyhieOGB_vY2-CrfFVYcHY6SjWMM10uyVCjtV7pJRfGbLK6ZUS4LzRv7ngTebVIduhouCVgVRiA57CdT1I_JNXE1CHlCorBmnNgEFIgANhLO9r62KQBZIIU6gtuBXpHmOBqUO5wWWBWmxJRIpT4YvGLwXpi75W2-qEUtGj1WcOOwqayT4I8z29I1QdnjkpRTaX_hr8ZXgLanaJxkLAqP_IMfMzxasjSrrW_eRzq1Qlvr-gm1M_HPISy4rhf_AcCi67ubk1BXIDF5DQoVt27h_R5S56FBGpOyEo1B8lD1QNpVMrP-MhzLVELc0z3aeU4fNdKFa7hs9AA"
 BOT_TOKEN = "1583090897:AAEIA5cK5hR5P4XY-b8t4TwBL8Dm4bvE3yU"
+SQLALCHEMY_DATABASE_URI = "postgres://infotel:asdasdasd@database-1.cgjylhh6lfc.us-east-2.rds.amazonaws.com:5431/dbname"
 BOT_NAME = "music test"
 admins = {}
 API_ID = "5553433"
